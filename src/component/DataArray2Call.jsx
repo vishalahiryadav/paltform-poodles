c170ceb9bd088c79358508a2d522bccbfb841baf
Vishal Ahir <vishalahir8002@gmail.com>
2023-07-25 18:41:36 +0530

    25

--- src/component/DataArray2Call.jsx
+++ src/component/DataArray2Call.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 function DataArray2Call(props) {
-  return <div className="img_text">
+  return <div className="img_with_text col-12 col-md-6 col-lg-4">
   <div>{props.img}</div>
-  <div className='head_text'>
+  <div>
     <h4>{props.heading}</h4>
     <p>{props.paragraph}</p></div>
   </div>
--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,33 +2,32 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="footer_img">
-      {/* <img src={require("./a.svg").default} /> */}
-    <div className="footer d-flex ">
-      <div className="footer_section_left">
-        <h1>poodles</h1>
-        <p>
-          Our mission is to connect homeless pets in India with loving homes. We
-          offer stress-free pet adoption services .
-        </p>
-        <div className="contact d-flex">
-          <p>contact@poodles.in</p>
-          <p>BTH,Jaipur</p>
+    <div className="footer_end m-0">
+      <div className="footer d-flex">
+        <div className="footer_section_left">
+          <h1>poodles</h1>
+          <p>
+            Our mission is to connect homeless pets in India with loving homes.
+            We offer stress-free pet adoption services .
+          </p>
+          <div className="contact d-flex">
+            <p>contact@poodles.in</p>
+            <p>BTH,Jaipur</p>
+          </div>
+        </div>
+
+        <div className="footer_section_right">
+          <h4> Got Anything To share ? </h4>
+          <div className="input_container">
+            <input className="border-0 border rounded"></input>
+            <input className="border-0 border rounded"></input>
+            <input className="border-0 border rounded"></input>
+            <button className="btn border-0 border rounded">Connect Now</button>
+          </div>
         </div>
       </div>
-    
-    <div className="footer_section_right">
-        <h4> Got Anything To share ? </h4>
-        <div className="input_container">
-        <input className="border-0 border rounded"></input>
-        <input className="border-0 border rounded"></input>
-        <input className="border-0 border rounded"></input>
-        <button className="btn border-0 border rounded">Connect Now</button>
-        </div>
+      <p className="copyright">© 2023 Poodles petcare. All rights reserved</p>
       </div>
-     </div>
-     <p className="copyright">© 2023 Poodles petcare. All rights reserved</p>
-    </div>
   );
 }
 
